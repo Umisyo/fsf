2b16fba21d11722dd56361520f743fb556e4e9fc
Umisyo <s_kusunoki@yumemi.co.jp>
2019-08-23 12:43:09 +0900

    add: ログインしてるか否かのフラグを追加

--- store/auth.js
+++ store/auth.js
@@ -1,11 +1,13 @@
 export const state = () => ({
     user: {
-    }
+    },
+    isAuthenticated: false
 })
 
 export const mutations = {
     setUser(state, payload) {
-        state.user = payload
+        state.user = payload, 
+        state.isAuthenticated = true
     }
 }
 
